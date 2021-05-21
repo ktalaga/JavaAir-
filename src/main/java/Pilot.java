f6ca4f7b18ae5549a49bd94ce51541fc8a45279b
konrad <ktalaga90@gmail.com>
2021-05-21 19:43:52 +0100

    Pilot class added, rank taken from enum, properties added, getters and setters added, pilotHasName test passed

---
+++ src/main/java/Pilot.java
@@ -0,0 +1,27 @@
+public class Pilot extends Person {
+
+    private Rank rank;
+    private String licenseNumber;
+
+    public Pilot(String name, Rank rank, String licenseNumber) {
+        super(name);
+        this.rank = rank;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+}
