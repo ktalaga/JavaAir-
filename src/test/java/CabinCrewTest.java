f1888f61ad623d26fa474a22cd73aedee6c23965
konrad <ktalaga90@gmail.com>
2021-05-21 20:06:07 +0100

    CabinCrewTest class added, hasName, canChangeName, hasRank, canChangeRank tests added

---
+++ src/test/java/CabinCrewTest.java
@@ -0,0 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CabinCrewTest {
+
+    private CabinCrew cabinCrew_1;
+    private CabinCrew cabinCrew_2;
+    private CabinCrew cabinCrew_3;
+    private CabinCrew cabinCrew_4;
+
+    @Before
+    public void before(){
+        cabinCrew_1 = new CabinCrew("Mike Tyson", Rank.FLIGHT_ATTENDANT);
+        cabinCrew_2 = new CabinCrew("Lennox Lewis", Rank.FLIGHT_ATTENDANT);
+        cabinCrew_3 = new CabinCrew("Jon Jones", Rank.FLIGHT_ATTENDANT);
+        cabinCrew_4 = new CabinCrew("Jan Blachowicz", Rank.FLIGHT_ATTENDANT);
+    }
+
+    @Test
+    public void cabinCrewHasName(){
+        assertEquals("Jan Blachowicz", cabinCrew_4.getName());
+    }
+
+    @Test
+    public void canChangeCabinCrewName(){
+        cabinCrew_1.setName("Morris Moss");
+        assertEquals("Morris Moss", cabinCrew_1.getName());
+    }
+
+    @Test
+    public void cabinCrewHasRank(){
+        assertEquals(Rank.FLIGHT_ATTENDANT, cabinCrew_1.getRank());
+    }
+
+    @Test
+    public void canChangeCabinCrewRank(){
+        cabinCrew_1.setRank(Rank.CAPITAN);
+        assertEquals(Rank.CAPITAN, cabinCrew_1.getRank());
+    }
+}
