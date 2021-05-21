8aa90537f14e8e56b5a737f5b1339b37b65c1680
konrad <ktalaga90@gmail.com>
2021-05-21 20:08:35 +0100

    CabinCrew class added, extended from person, getters and setters added, all tests in CabinCrewTest passed

---
+++ src/main/java/CabinCrew.java
@@ -0,0 +1,17 @@
+public class CabinCrew extends Person{
+
+    private Rank rank;
+
+    public CabinCrew(String name, Rank rank) {
+        super(name);
+        this.rank = rank;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+}
